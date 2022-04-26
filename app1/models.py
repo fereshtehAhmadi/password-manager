9c4bebbec4696af1c788956ba6d51e686526103e
freshtehAhmadi <freshtehahmadi79@gmail.com>
2022-04-26 18:38:40 +0430

    make base.html

--- app1/models.py
+++ app1/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class PasswordModel(models.Model):
-    title = models.CharField(max_length=64)
+    title = models.CharField(max_length=100)
     username = models.CharField(max_length=64)
     password = models.CharField(max_length=64)
 
